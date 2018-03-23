7440a389c8bc813c8ab6aad14b5154b149a933bb
madhuri777 <satkarmadhuri777@gmail.com>
2018-03-23 15:01:32 +0530

    commit

--- src/com/bridgeit/programs/LeapYear.java
+++ src/com/bridgeit/programs/LeapYear.java
@@ -1,14 +1,14 @@
 package com.bridgeit.programs;
 
-//import com.bridgeit.utility.Utility;
+import com.bridgeit.utility.Utility;
 
 public class LeapYear {
 
 	public static void main(String[] args) {
-		//Utility un=new Utility();
+		Utility utility=new Utility();
 		System.out.println("Enter the year");
-	   // int yr=un.inputInteger();
-	    
-	}
+	    int year=utility.inputInteger();
+	    Utility.leapYearMethod(year);
+	    }
 
 }
